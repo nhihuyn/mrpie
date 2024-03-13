4381c3eb24690d4ecf16577fe01eec22b18bb511
NhiHuynh <nhihuynh@quintet.co.kr>
2024-03-13 08:09:23 +0700

    feature-T01: setting structure source code

---
+++ mr.pie/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import { useTranslation } from 'react-i18next';
+// import Header from './components/common/header/header';
+import Footer from './components/common/footer/footer';
+function App() {
+  return (
+    // <Header/>
+    <Footer />
+  );
+}
+
+export default App;
