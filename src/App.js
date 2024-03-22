cb243a26810ed4e55523e22c49b74ca5b461c3e5
NhiHuynh <nhihuynh@quintet.co.kr>
2024-03-22 15:01:57 +0700

    feat:TC create browser router for app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
+// import Login from './components/Login/login';
 import DetailProduct from './components/common/detail-product/detailProduct';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useTranslation } from 'react-i18next';
-// import Header from './components/common/header/header';
-// import Footer from './components/common/footer/footer';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from './components/common/header/header';
+import Footer from './components/common/footer/footer';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <Header />
+        {/* <Root /> */}
+        <Footer />
+      </div>
+    ),
+    errorElement: <div>Error</div>,
+  },
+  // {
+  //   path: '/login',
+  //   element: <Login />,
+  // },
+  {
+    path: '/products/:productId',
+    element: <DetailProduct />,
+  },
+])
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<DetailProduct />}>
-        <Route index element={<DetailProduct />} />
-        <Route path="blogs" element={<DetailProduct />} />
-        <Route path="contact" element={<DetailProduct />} />
-        <Route path="*" element={<DetailProduct />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    // <Provider store={store}>
+      <RouterProvider router={router} />
+    // </Provider>
   );
 }
 
