8aeb554a64c991cbe2ad4b9f3d43ebeaa68b9046
Thangdo <doquangthang15@gmail.com>
2024-03-26 19:56:57 +0700

    fea:P06 commit detail item page

--- src/components/common/Detail/Detail.tsx
+++ src/components/common/Detail/Detail.tsx
@@ -1,11 +1,13 @@
 
 import React, { useState } from 'react';
-import { HeartOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
-import { Rate, Alert } from 'antd';
-import veganImage from '../../../assets/images/vegan_thaicury.png';
-import background from '../../../assets/images/background_vegan.png';
+import { HeartOutlined, ExclamationCircleOutlined, RightOutlined } from '@ant-design/icons';
+import { Rate, Alert, Select } from 'antd';
+//import { vegan, bg_vegan } from '../../../assets/images/index';
+import vegan from '../../../assets/images/vegan_thaicury.png';
+import bg_vegan from '../../../assets/images/background_vegan.png';
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
+
 
 const Detail: React.FC = () => {
   const { t } = useTranslation();
@@ -225,58 +227,77 @@ const Detail: React.FC = () => {
 )}
 
       <div className="bg-black text-white p-4">
-        <p>{t('Home')} &gt; {t('Menu')} &gt; {t('ColdCakes')} &gt; <strong>Vegan Thai Cury</strong></p>
+        <p>{t('Home')} <RightOutlined className="text-sm"/> {t('Menu')} <RightOutlined  className="text-sm"/>  {t('ColdCakes')} <RightOutlined  className="text-sm"/>  <strong>Vegan Thai Cury</strong></p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 bg-gray-200 justify-center items-center"  
-        style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', minHeight: '100vh', position: 'relative' }}>
+        style={{ backgroundImage: `url(${bg_vegan})`, backgroundSize: 'cover', minHeight: '100vh', position: 'relative' }}>
         
         <div className="w-full lg:ml-40 p-8 text-white justify-center items-center">
-          <h1 className="text-3xl font-bold mb-4">Vegan Thai Cury</h1>
-          <p className="text-sm mb-4">{t('ProductDescriptionShort')}</p>
+          <h1 className="text-3xl md:text-5xl font-bold mb-6">Vegan Thai Cury</h1>
+          <p className="text-sm md:text-base mb-6">{t('ProductDescriptionShort')}</p>
 
-          <div className="flex items-center mb-4">
-            <p className="text-sm mr-2">{t('Phân loại')}:</p>
-            <select 
+          <div className="flex items-center mb-6">
+            <p className="text-sm md:text-base mr-2">{t('Phân loại')}:</p>
+            {/* <select 
               className="bg-gray-200 text-black p-2 px-6 rounded-md mr-4"
               onChange={(e) => setSelectedCategory(e.target.value)}
-              >
-              <option value="">{t('SelectCakeType')}</option>
-              <option value="category1">{t('HotCakes')}</option>
-              <option value="category2">{t('ColdCakes')}</option>
-              </select>
-              </div>
+            >
+                <option value="">{t('SelectCakeType')}</option>
+                <option value="category1">{t('HotCakes')}</option>
+                <option value="category2">{t('ColdCakes')}</option>
+            </select> */}
+            <Select
+              style={{ width: 200 }}
+              placeholder={t('SelectCakeType')}
+              optionFilterProp="children"
+              filterOption={(input, option) => (option?.label ?? '').includes(input)}
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+              }
+              options={[
+                {
+                  value: '2',
+                  label: t('HotCakes'),
+                },
+                {
+                  value: '3',
+                  label: t('ColdCakes'),
+                }
+              ]}
+            />
+          </div>
 
-              <div className="flex items-center mb-4">
-        <div className="border border-white flex ">
-          <button onClick={handleDecreaseCount} className="p-2 px-4 hover:bg-red-400">-</button>
-          <input type="text" className="p-2 w-16 border-1 text-center text-black" value={count} readOnly />
-          <button onClick={handleIncreaseCount} className="p-2 px-4 hover:bg-red-400">+</button>
+        <div className="flex items-center mb-6">
+          <div className="border border-white flex rounded-3xl">
+            <button onClick={handleDecreaseCount} className="p-2 px-4 hover:bg-red-400 rounded-l-3xl">-</button>
+            <input type="text" className="p-2 w-16 border-1 text-center text-black" value={count} readOnly />
+            <button onClick={handleIncreaseCount} className="p-2 px-4 hover:bg-red-400 rounded-r-3xl">+</button>
+          </div>
+          <HeartOutlined
+            onClick={handleAddToFavorite}
+            className={`h-10 w-10 ml-4 text-red-500 border-2 border-gray-400 hover:bg-red-200 bg-white rounded-full flex items-center justify-center cursor-pointer ${favorite ? 'fill-current' : ''}`}
+          />
         </div>
-        <HeartOutlined
-          onClick={handleAddToFavorite}
-          className={`h-10 w-10 ml-4 text-red-500 border-2 border-gray-400 hover:bg-red-200 bg-white rounded-full flex items-center justify-center cursor-pointer ${favorite ? 'fill-current' : ''}`}
-        />
-      </div>
 
-      <div className="flex items-center mb-4">
-        <p className="text-sm mr-2">{t('Size')}:</p>
+      <div className="flex items-center mb-6">
+        <p className="text-sm md:text-base mr-4">{t('Size')}:</p>
         <div className="flex">
           <button 
             onClick={() => handleSizeSelect("S")} 
-            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-2 ${selectedSizes['S'] ? 'bg-red-400 text-white' : ''}`}
+            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-3 ${selectedSizes['S'] ? 'bg-red-400 text-white' : ''}`}
           >
             S
           </button>
           <button 
             onClick={() => handleSizeSelect("M")} 
-            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-2 ${selectedSizes['M'] ? 'bg-red-400 text-white' : ''}`}
+            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-3 ${selectedSizes['M'] ? 'bg-red-400 text-white' : ''}`}
           >
             M
           </button>
           <button 
             onClick={() => handleSizeSelect("L")} 
-            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-2 ${selectedSizes['L'] ? 'bg-red-400 text-white' : ''}`}
+            className={`hover:bg-red-400 border border-white font-bold text-sm px-3 py-2 rounded-md mr-3 ${selectedSizes['L'] ? 'bg-red-400 text-white' : ''}`}
           >
             L
           </button>
@@ -289,24 +310,24 @@ const Detail: React.FC = () => {
         </div>
       </div>
 
-      <button onClick={handleAddToCart} className="bg-red-500 hover:bg-red-400 text-white px-4 py-4 rounded-md mt-4">{t('AddToCart')} &gt;</button>
+      <button onClick={handleAddToCart} className="bg-red-500 hover:bg-red-400 text-white px-4 py-4 rounded-3xl mt-4">{t('AddToCart')} <RightOutlined className="text-sm"/></button>
     </div>
 
-    <div className="w-full md:w-1/2 flex flex-col items-center md:ml-20 mt-4 md:mt-0 relative z-20">
-      <img src={veganImage} alt="Bánh" className="w-3/5 md:w-auto" />
+    <div className="w-full md:w-1/2 flex flex-col items-center md:ml-20 md:mt-28 relative z-20">
+      <img src={vegan} alt="Bánh" className="w-3/5 md:w-auto" />
     </div>
   </div>
 
-  <div>
-    <div className="flex justify-start border-t border-gray-300 mt-8 ml-8">
-      <button onClick={() => setActiveTab("description")} className={` ${activeTab === "description" ? "text-red-500 border-b-2 border-t-2  border-red-500" : "text-gray-500"}`}>{t('Description')}</button>
-      <button onClick={() => setActiveTab("review")} className={`mx-6 ${activeTab === "review" ? "text-red-500 border-b-2 border-t-2 border-red-500" : "text-gray-500"}`}>{t('Reviews')}</button>
+  <div className="py-4 px-8 md:py-8 md:px-48">
+    <div className="flex justify-start">
+      <button onClick={() => setActiveTab("description")} className={`px-8 py-2 ${activeTab === "description" ? "text-red-500 border-b-2 border-red-500 font-bold" : "text-gray-500"}`}>{t('Description')}</button>
+      <button onClick={() => setActiveTab("review")} className={`mx-6 px-8 py-2 ${activeTab === "review" ? "text-red-500 border-b-2 border-red-500 font-bold" : "text-gray-500"}`}>{t('Reviews')}</button>
     </div>
 
-    <div className="mt-8 ml-8 mr-8 min-h-8 ">
+    <div className="mt-8 mr-8 min-h-8 ">
       {activeTab === "description" && (
         <>
-          <h2 className="text-2xl font-bold mb-2">{t('ProductInformation')}</h2>
+          <p className="text-xl md:text-2xl mb-2">{t('ProductInformation')}</p>
           <ul className="list-disc pl-4">
             <li>Vegan Thai Cury</li>
             <li>Cold Cakes</li>
@@ -315,35 +336,35 @@ const Detail: React.FC = () => {
         </>
       )}
 
-{activeTab === "review" && (
-  <>
-    <h2 className="md:text-2xl mb-2">{t('AddYourComment')}</h2>
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="md:col-span-2 mt-4 flex items-center mb-4">
-        <p className="sm:text-sm mr-4 mb-2 md:mb-0">{t('YourRating')}:</p>
-        <Rate allowHalf onChange={setRating} value={rating} className="mr-4" />
-      </div>
-      <div className="flex flex-col md:flex-row md:col-span-1 mb-4">
-        <div className="mb-2 md:mr-4">
-          <p className="text-sm mb-2">{t('Username')}</p>
-          <input type="text" className="p-2 border border-gray-300 rounded-md w-full" value={userName} onChange={(e) => setUserName(e.target.value)} />
+    {activeTab === "review" && (
+      <>
+        <h2 className="text-xl md:text-2xl mb-6">{t('AddYourComment')}</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="md:col-span-2 flex items-center mb-6">
+            <p className="sm:text-sm mr-4 mb-2 md:mb-0">{t('YourRating')}:</p>
+            <Rate allowHalf onChange={setRating} value={rating} className="mr-4" />
+          </div>
+          <div className="flex flex-col md:flex-row md:col-span-1">
+            <div className="mb-6 md:mr-4">
+              <p className="text-sm mb-2">{t('Username')}</p>
+              <input type="text" className="p-2 border border-gray-300 rounded-md w-full" value={userName} onChange={(e) => setUserName(e.target.value)} />
+            </div>
+            <div className="mb-6 md:mr-4">
+              <p className="text-sm mb-2">{t('PhoneNumber')}</p>
+              <input type="tel" className="p-2 border border-gray-300 rounded-md w-full" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+            </div>
+          </div>
+          <div className="md:col-span-2 mb-6">
+            <p className="text-sm mb-2">{t('ReviewContent')}</p>
+            <textarea rows={4} className="p-6 border border-gray-300 rounded-md w-full" value={reviewContent} onChange={(e) => setReviewContent(e.target.value)} />
+          </div>
+          <div className="md:col-span-2 flex justify-center">
+            <button onClick={handleReviewSubmit} className="bg-red-500 text-white px-4 py-2 rounded-md">{t('SubmitComment')}</button>
+          </div>
         </div>
-        <div>
-          <p className="text-sm mb-2">{t('PhoneNumber')}</p>
-          <input type="tel" className="p-2 border border-gray-300 rounded-md w-full" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+        </>
+      )}
         </div>
-      </div>
-      <div className="md:col-span-2">
-        <p className="text-sm mt-4 mb-2">{t('ReviewContent')}</p>
-        <textarea rows={4} className="p-6 border border-gray-300 rounded-md w-full" value={reviewContent} onChange={(e) => setReviewContent(e.target.value)} />
-      </div>
-       <div className="md:col-span-2 mt-6 flex justify-center">
-        <button onClick={handleReviewSubmit} className="bg-red-500 text-white px-4 py-2 mb-6 rounded-md">{t('SubmitComment')}</button>
-       </div>
-     </div>
-    </>
-  )}
-    </div>
   </div>
 </div>
 );
