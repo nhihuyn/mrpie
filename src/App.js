97c6a39728e7e779c5c5c869f3568623383ae941
pvdkhoa <phamvudangkhoa2002@gmail.com>
2024-03-29 11:03:19 +0700

    fea:P04-Mainpage Complete 70% UI page

--- src/App.js
+++ src/App.js
@@ -10,23 +10,26 @@ import Login from './components/common/Login/Login';
 import ForgotPassword from './components/common/ForgotPasswd/ForgotPasswd';
 import RegisterForm from "./components/common/register/register";
 import Checkout from "./components/common/checkout/checkout";
+import Mainpage from "./components/common/mainpage/mainpage";
+import Header from "./components/common/header/header";
 
 function App() {
   return (
-    // <Router>
-    //   <Layout>
-    //       <Routes>
-    //         <Route path="/" element={<DetailProduct/>} />
-    //         <Route path="/products/:productId" element={<Menu/>} />
-    //         <Route path="/change_password" element={<ChangePasswd/>} />
-    //         <Route path="/login" element={<Login/>} />
-    //         <Route path="/forgot_password" element={<ForgotPassword/>} />
-    //       </Routes>
-    //   </Layout>
-    // </Router>
-    // <RegisterForm/>
-    <Checkout/>
-
+    <Router>
+      <Layout>
+          <Routes>
+            <Route path="/" element={<DetailProduct/>} />
+            <Route path="/products/:productId" element={<Menu/>} />
+            <Route path="/change_password" element={<ChangePasswd/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/forgot_password" element={<ForgotPassword/>} />
+            <Route path="/mainpage" element={<Mainpage/>} />
+          </Routes>
+     </Layout>
+    </Router>
+   
+    
+    
   );
 }
 
