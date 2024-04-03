6ad4c90a759204413f9310e180c0dca3e832cc22
pvdkhoa <phamvudangkhoa2002@gmail.com>
2024-04-03 09:16:14 +0700

    fea:P11-Checkout update i18n file and change icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,8 @@
 export { default as bakery1 } from './bakery1.png'
 export { default as badge } from './badge.png'
 export { default as logo } from './logo.png'
+export { default as pizza } from './pizza.jpg'
+export { default as bread } from './bread.jpg'
+export { default as deliveryIcon } from './delivery.svg'
+export { default as qrCode } from './QRcode.svg'
+export { default as moneyIcon} from './shop.svg'
