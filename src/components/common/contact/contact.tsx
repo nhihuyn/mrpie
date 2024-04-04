2999d03797c5af98447845862317a308d144c5a7
NhiHuynh <nhihuynh@quintet.co.kr>
2024-04-04 11:41:29 +0700

    feat:T0 making mockup UI

---
+++ src/components/common/contact/contact.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Map from "../mainpage/components/map"
+
+const Contact = () => {
+    return (
+        <div className="w-full h-96">
+            <Map />
+      </div>
+    )
+}
+
+export default Contact
